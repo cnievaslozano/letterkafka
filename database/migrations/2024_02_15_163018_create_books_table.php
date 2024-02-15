714dab416dea87f9c2ac9799961f578936aa8662
cnievaslozano <cnievaslozano@gmail.com>
2024-02-15 17:48:48 +0100

    book migrate

--- database/migrations/2024_02_15_163018_create_books_table.php
+++ database/migrations/2024_02_15_163018_create_books_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,19 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('author_first_name');
+            $table->string('author_last_name');
+            $table->string('review_name');
+            $table->text('review_body');
+            $table->string('external_id');
+            $table->integer('book_id')->unsigned();
+            $table->string('title');
+            $table->integer('pages')->unsigned();
+            $table->json('genres');
+            $table->float('rating', 4, 2);
+            $table->text('plot');
+            $table->string('cover');
+            $table->text('url');
             $table->timestamps();
         });
     }
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use GuzzleHttp\Client;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
@@ -12,7 +13,46 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $client = new Client();
 
+        $existingBookIds = Book::pluck('book_id')->toArray();
+        $successfulRequests = 0;
+
+        for ($i = 0; $i < 1000; $i++) {
+            $response = $client->request('GET', 'https://books-api7.p.rapidapi.com/books/get/random/', [
+                'headers' => [
+                    'X-RapidAPI-Host' => 'books-api7.p.rapidapi.com',
+                    'X-RapidAPI-Key' => '74f7b80806mshe51d26471ed199fp17cdfdjsn83face454e73',
+                ],
+            ]);
+
+            $bookData = json_decode($response->getBody(), true);
+
+            // Verificar si la clave "review" existe en el arreglo
+            if (array_key_exists('review', $bookData)) {
+                if (!in_array($bookData['book_id'], $existingBookIds)) {
+                    Book::create([
+                        'author_first_name' => $bookData['author']['first_name'],
+                        'author_last_name' => $bookData['author']['last_name'],
+                        'review_name' => $bookData['review']['name'],
+                        'review_body' => $bookData['review']['body'],
+                        'external_id' => $bookData['_id'],
+                        'book_id' => $bookData['book_id'],
+                        'title' => $bookData['title'],
+                        'pages' => $bookData['pages'],
+                        'genres' => json_encode($bookData['genres']),
+                        'rating' => $bookData['rating'],
+                        'plot' => $bookData['plot'],
+                        'cover' => $bookData['cover'],
+                        'url' => $bookData['url'],
+                    ]);
+
+                    $existingBookIds[] = $bookData['book_id']; // Agrega el nuevo book_id a la lista de ids existentes
+                    $successfulRequests++;
+                }
+            }
+        }
+
+        echo "Se guardaron $successfulRequests libros en la base de datos.";
     }
 }
