a71e42a089083d5f4cdb72c138185539931744a8
cnievaslozano <cnievaslozano@gmail.com>
2024-02-16 22:24:41 +0100

    campos base books

--- database/migrations/2024_02_15_163018_create_books_table.php
+++ database/migrations/2024_02_15_163018_create_books_table.php
@@ -4,36 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+class CreateBooksTable extends Migration
+{
+    public function up()
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('author_first_name');
-            $table->string('author_last_name');
-            $table->string('review_name');
-            $table->text('review_body');
-            $table->string('external_id');
-            $table->integer('book_id')->unsigned();
-            $table->string('title');
-            $table->integer('pages')->unsigned();
-            $table->json('genres');
-            $table->float('rating', 4, 2);
-            $table->text('plot');
-            $table->string('cover');
-            $table->text('url');
+            $table->string('etag')->nullable();
+            $table->string('selflink')->nullable();
+            $table->text('title')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->json('authors')->nullable();
+            $table->json('genres')->nullable();
+            $table->string('publishedDate')->nullable();
+            $table->text('description')->nullable();
+            $table->json('industryIdentifiers')->nullable();
+            $table->integer('pageCount')->nullable();
+            $table->string('maturityRating')->nullable();
+            $table->json('imageLinks')->nullable();
+            $table->string('language')->nullable();
+            $table->string('previewLink')->nullable();
+            $table->string('infoLink')->nullable();
+            $table->string('accessInfo_country')->nullable();
+            $table->json('epub')->nullable();
+            $table->string('webReaderLink')->nullable();
+            $table->text('textSnippet')->nullable();
+            $table->string('amazon_product_url')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('books');
     }
-};
+}
