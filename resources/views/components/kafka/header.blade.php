d147fdf98146837e4fb0e2e7d36ef2ae5a5877a8
cnievaslozano <cnievaslozano@gmail.com>
2024-03-03 21:43:30 +0100

    header component

---
+++ resources/views/components/kafka/header.blade.php
@@ -0,0 +1,11 @@
+<h1>Letter Kafka
+    <x-kafka.logo width="120" />
+</h1>
+<header>
+    <ul>
+        <li><a href="#">Libros</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Mis estanterías</a></li>
+        <li><a href="#">Entrar</a></li>
+    </ul>
+</header>
