e3f1c462a251a99ef9ab95487fc462da6c6c5249
cnievaslozano <cnievaslozano@gmail.com>
2024-03-03 13:23:25 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
