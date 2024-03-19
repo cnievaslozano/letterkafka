cfba3e34365a1cd623ce9b3a7484a266f7002357
cnievaslozano <cnievaslozano@gmail.com>
2024-03-19 17:39:59 +0100

    page libros

---
+++ resources/views/libros.blade.php
@@ -0,0 +1,32 @@
+<x-kafka-layout>
+    <x-kafka.header />
+
+    <section class="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <!-- Columna izquierda: libros -->
+        <div class="col-span-2 md:col-span-2">
+            <h2 class="mt-4 mb-4">Libros Destacados</h2>
+            <hr class="bg-black">
+            <div class="mt-8 grid grid-cols-1 gap-4"> 
+                @foreach ($books as $book)
+                    <div class="col-span-2 md:col-span-1">
+                        <x-kafka.card 
+                            title="{{ $book->title }}" 
+                            imageUrl="{{ $book->cover }}"
+                            autor="{{ $book->author_first_name ? $book->author_first_name : ' ' }} {{ $book->author_last_name ? $book->author_last_name : ' ' }}"
+                            duration="{{ $book->pages }} pág" 
+                            genres="{{ $book->genres }}" 
+                            description="{{ $book->plot }}" 
+                            rating="{{ $book->rating }}" 
+                            mood="{{ $book->mood }}" 
+                        />
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <!-- Columna derecha: filtros -->
+        <div class="col-span-1 md:col-span-1">
+            <x-kafka.filter />
+        </div>
+    </section>
+</x-kafka-layout>
