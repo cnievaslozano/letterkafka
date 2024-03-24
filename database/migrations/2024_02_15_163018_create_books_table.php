f64144af7ae2756c95cc7a2b33cc3e0b62180eb7
cnievaslozano <cnievaslozano@gmail.com>
2024-03-24 19:32:51 +0100

    Migración simple y seeder

--- database/migrations/2024_02_15_163018_create_books_table.php
+++ database/migrations/2024_02_15_163018_create_books_table.php
@@ -10,28 +10,18 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('etag')->nullable();
-            $table->string('selflink')->nullable();
-            $table->text('title')->nullable();
-            $table->string('subtitle')->nullable();
-            $table->json('authors')->nullable();
-            $table->json('genres')->nullable();
-            $table->string('publishedDate')->nullable();
-            $table->text('description')->nullable();
-            $table->json('industryIdentifiers')->nullable();
-            $table->integer('pageCount')->nullable();
-            $table->string('maturityRating')->nullable();
-            $table->json('imageLinks')->nullable();
-            $table->string('language')->nullable();
-            $table->string('previewLink')->nullable();
-            $table->string('infoLink')->nullable();
-            $table->string('accessInfo_country')->nullable();
-            $table->json('epub')->nullable();
-            $table->string('webReaderLink')->nullable();
-            $table->text('textSnippet')->nullable();
-            $table->string('amazon_product_url')->nullable();
+            $table->string('author_first_name');
+            $table->string('author_last_name');
+            $table->string('title');
+            $table->integer('pages');
+            $table->string('genres');
+            $table->float('rating');
+            $table->text('plot');
+            $table->string('cover')->nullable();
+            $table->string('url')->nullable();
             $table->timestamps();
         });
+        
     }
 
     public function down()
