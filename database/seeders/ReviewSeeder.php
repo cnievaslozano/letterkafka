42d20677bbc2696c9a287ec85c326fc19ee59606
cnievaslozano <cnievaslozano@gmail.com>
2024-03-27 21:09:43 +0100

    add field: userImage & likes

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -1,6 +1,5 @@
 <?php
-
-// database/seeders/ReviewSeeder.php
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +17,9 @@ class ReviewSeeder extends Seeder
                 'username' => $faker->name,
                 'createdAt' => $faker->dateTimeThisMonth(),
                 'content' => $faker->paragraph,
+                'userImage' => $faker->imageUrl($width = 24, $height = 24),
                 'portada' => $faker->imageUrl($width = 32, $height = 48),
+                'likes' => $faker->numberBetween(0, 100),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
