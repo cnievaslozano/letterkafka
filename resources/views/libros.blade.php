40e29523faeb7f78d00cf946130c3f0d5d89a1d6
cnievaslozano <cnievaslozano@gmail.com>
2024-03-27 21:13:54 +0100

    modulation

--- resources/views/libros.blade.php
+++ resources/views/libros.blade.php
@@ -4,8 +4,7 @@
     <section class="grid grid-cols-2 md:grid-cols-3 gap-4">
         <!-- Columna izquierda: libros -->
         <div class="col-span-2 md:col-span-2">
-            <h2 class="mt-4 mb-4">Libros Destacados</h2>
-            <hr class="bg-black">
+            <h2 class="mt-4 mb-4 text-center">Libros Destacados</h2>
             <div class="mt-8 grid grid-cols-1 gap-4"> 
                 @foreach ($books as $book)
                     <div class="col-span-2 md:col-span-1">
@@ -22,10 +21,12 @@
                     </div>
                 @endforeach
             </div>
+            {{ $books->links('components.kafka.pagination') }}
         </div>
 
         <!-- Columna derecha: filtros -->
         <div class="col-span-1 md:col-span-1">
+            <x-kafka.search />
             <x-kafka.filter />
         </div>
     </section>
