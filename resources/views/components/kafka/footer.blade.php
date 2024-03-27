2ff58c1b413e145ec811f24c5c5023ebb82cbd26
cnievaslozano <cnievaslozano@gmail.com>
2024-03-27 21:15:28 +0100

    fix width

--- resources/views/components/kafka/footer.blade.php
+++ resources/views/components/kafka/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class=" w-full max-w-screen-xl mx-auto p-4 md:py-8 ">
+<footer class=" w-full max-w-screen-xl mx-auto md:py-8  bottom-0 left-0 right-0">
     <div class="sm:flex sm:items-center sm:justify-between">
         <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
             <x-kafka.logo width="35" />
@@ -16,8 +16,8 @@
             </li>
         </ul>
     </div>
-    <hr>
-    <span class="block text-sm  sm:text-center ">© 2024 <a href="#" class="hover:underline">LetterKafka™</a> Todos
+    <hr class="bg-black">
+    <span class="block text-sm  sm:text-center ">© 2024 <a href="/" class="hover:underline">LetterKafka™</a> Todos
         los derechos reservados.</span>
 
 </footer>
