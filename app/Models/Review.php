ddfcbbd7ad99d70cd3e76d566316eaae8994d381
cnievaslozano <cnievaslozano@gmail.com>
2024-03-27 21:09:22 +0100

    Review Model

---
+++ app/Models/Review.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user', 'userImage', 'username', 'createdAt', 'content', 'portada'];
+
+}
