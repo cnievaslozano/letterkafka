9b9b117f58e00df02f68e9b64ecf5bc3f5c804a0
cnievaslozano <cnievaslozano@gmail.com>
2024-03-27 21:12:52 +0100

    Adapt Content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,117 +1,139 @@
 <x-kafka-layout>
+    <main class="main__landing">
+        <x-kafka.header />
+        <h2 class="title--large main-title">Lleva un registro de los libros que has devorado, los que aún no has
+            explorado... y aquellos que no puedes esperar para sumergirte en ellos
+        </h2>
+        <div class="main-text multi-column">
+            <p>Imagina un jardín de letras, donde cada libro es una <span class="font-bold">flor</span> distinta, cada
+                página una <span class="font-bold">pétalo</span> vibrante de
+                <span class="font-bold">historias</span> y <span class="font-bold">emociones</span>. Esta aplicación es
+                como
+                un <span class="font-bold">invernadero</span> virtual, donde puedes cultivar tu propio
+                rincón <span class="font-bold">literario</span>, compartiendo tus <span
+                    class="font-bold">descubrimientos</span> con amigos y explorando nuevos <span
+                    class="font-bold">mundos</span> entre estanterías
+                digitales.
+            </p>
+            <p>Desde la <span class="font-bold">posibilidad</span> de crear <span class="font-bold">reseñas</span>
+                detalladas de tus libros favoritos hasta descubrir nuevas <span class="font-bold">lecturas</span>
+                recomendadas por la comunidad, cada página que recorras se convierte en una <span
+                    class="font-bold">oportunidad</span> para conectar con
+                otros amantes de la <span class="font-bold">lectura</span>. Además, con la <span
+                    class="font-bold">capacidad</span> de crear <span class="font-bold">listas</span> pendientes de
+                <span class="font-bold">lectura</span> personalizadas,
+                nunca perderás de <span class="font-bold">vista</span> esos libros que siempre has querido explorar.
+            </p>
+            <p> Tendrás <span class="font-bold">acceso</span> a una amplia <span class="font-bold">red</span> de <span
+                    class="font-bold">lectores</span> con gustos similares, donde puedes seguir a tus amigos y ver sus
+                <span class="font-bold">lecturas</span> recientes en tiempo real. Esta función te permite no solo
+                explorar
+                nuevas obras a través de las
+                <span class="font-bold">recomendaciones</span> de tus conocidos, sino también compartir tus propias
+                <span class="font-bold">experiencias</span> <span class="font-bold">literarias</span> y
+                <span class="font-bold">descubrimientos</span> más recientes.
+            </p>
 
-    <h2 class="title--large main-title">Lleva un registro de los libros que has devorado, los que aún no has
-        explorado... y aquellos que no puedes esperar para sumergirte en ellos
-    </h2>
-    <div class="main-text multi-column">
-        <p>Imagina un jardín de letras, donde cada libro es una flor distinta, cada página una pétalo vibrante de
-            historias y emociones. Esta aplicación es como un invernadero virtual, donde puedes cultivar tu propio
-            rincón literario, compartiendo tus descubrimientos con amigos y explorando nuevos mundos entre estanterías
-            digitales.</p>
-        <p>Desde la posibilidad de crear reseñas detalladas de tus libros favoritos hasta descubrir nuevas lecturas
-            recomendadas por la comunidad, cada página que recorras se convierte en una oportunidad para conectar con
-            otros amantes de la lectura. Además, con la capacidad de crear listas pendientes de lectura personalizadas,
-            nunca perderás de vista esos libros que siempre has querido explorar.</p>
-        <p> Tendrás acceso a una amplia red de lectores con gustos similares, donde puedes seguir a tus amigos y ver sus
-            lecturas recientes en tiempo real. Esta función te permite no solo explorar nuevas obras a través de las
-            recomendaciones de tus conocidos, sino también compartir tus propias experiencias literarias y
-            descubrimientos más recientes.</p>
-    </div><a class="terrarium" href="https://codepen.io/oliviale/full/oNXJgEM" target="_blank">
-        <figure><img src="{{ asset('images/logos/home/library.jpg') }}" />
-            <figcaption> </figcaption>
-        </figure>
-    </a><a class="item-with-image plan span--2 long--2" href="https://codepen.io/oliviale/full/moLrBq"
-        target="_blank"><img src="{{ asset('images/logos/home/power-of-literature.jpg') }}" />
-        <h4>¡Desbloquea el Poder de la Versión Pro! </h4>
-        <div class="multi-column">
-            <p>LetterKafka es gratis, pero quizás te interesa la versión PRO. Una versión que va más allá, donde no habrá anuncios, donde tendrás un wrapped anual y ofertas únicas en tús librerías de confianza. Aparte desbloquearás una personalización de perfil más avanzada.</p>
         </div>
-    </a><a class="hogwarts" href="https://codepen.io/oliviale/full/MZZYyO" target="_blank">
-        <div class="hogwarts__title">25% Menos en la versión PRO</div>
-        <div class="hogwarts__image"> <span>Oferta limitada</span><img
-                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-hogwarts.svg" /></div>
-    </a><a class="item-with-image pasta with-border" href="https://codepen.io/oliviale/full/zYvxXWw" target="_blank">
-        <h4>GRATIS</h4>
-        <p>Everybody loves pasta. It's impossible to not love them, so here are 15 recipes, served with GSAP.
-            Note the easter egg — the menu icon changes according to the pasta that you choose.</p>
-    </a><a class="item-with-image magazine with-border" href="https://codepen.io/oliviale/full/GRKQoKM" target="_blank">
-        <h4>PRO</h4>
-        <p>A tribute of sorts to an era of cool superhero movies, this is a Marvel-themed magazine! Recommended,
-            because it took 45 hours, so you know some serious dedication has been poured into this.</p>
-    </a><a class="item-with-image style" href="https://codepen.io/collection/XJyNPm" target="_blank">
-        <h4>Let's adopt, don't shop.</h4>
-        <p>Explore a style guide and various page layouts for a pet adoption site in the<em>Style Guide</em>
-            collection. Inspired by the<em>Atomic Design</em>
-            principle, this 8-pen collection includes a 404 page concept, an admin dashboard and what a Twitter
-            user calls a 'media card zen garden'. Each pen comes with five (yes, five) color themes, including
-            the precious dark mode.
-        </p>
-    </a><a class="item-with-image toggles" href="https://codepen.io/oliviale/full/xxboXzo" target="_blank"><img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-toggle.gif" />
-        <h4>Toggles</h4>
-        <p>Eight playful toggles to toggle on and off for your amusement. Is it possible to use these on your
-            site and still adhere to assessibility rules? No. But there's a really cute dog one
-            you<em>have</em>to see.</p>
-    </a><a class="menu" href="https://codepen.io/oliviale/full/LYYrKMV" target="_blank">
-        <figure><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-menu.jpg" />
-            <figcaption>See the new and improved menu for Toasty!</figcaption>
-        </figure>
-    </a><a class="social" href="https://codepen.io/oliviale/full/QWjjrZX" tagrget="_blank"><img class="social__image"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-social.gif" />
-        <div class="social__subtitle">World News</div>
-        <div class="social__content">Looks like Covid-19 is gonna be around for a while so here is another
-            friendly reminder to practice social distancing. Oh, and wear a mask!</div>
-    </a>
-    <div class="item-with-image cssgrid-collection"><a class="cssgrid-collection__image"
-            href="https://www.planetadelibros.com/blog/actualidad/15/nuestros-tops/11/articulo/beneficios-de-leer/237"
-            target="_blank"><img src="https://www.corachan.com/images/63901/facebook.jpeg" /></a>
-        <div class="cssgrid-collection__content">
-            <h4> <a href="https://www.planetadelibros.com/blog/actualidad/15/nuestros-tops/11/articulo/beneficios-de-leer/237"
-                    target="_blank">10 sorprendentes beneficios de leer a
-                    diario <figcaption>(fuente: planetadelibros.com)</figcaption></a></h4>
-            <div class="multi-column-3">
-                <p>
-                    Se suele decir que el perro es el mejor amigo del humano. Sin robarle amor a nuestros amigos de
-                    cuatro patas, nosotros nos alineamos más con lo que decía Ernest Hemingway: «jamás habrá amigo más
-                    fiel que un libro». En los últimos tiempos los índices de consumo de libros han aumentado e incluso
-                    hemos incorporado nuevos formatos como el audiolibro a nuestros hábitos. La lectura es nuestra forma
-                    de evasión con mayúsculas, cuando por enfermedad u otras causas nos fallan el resto de recursos. Un
-                    libro nos permite ampliar nuestros horizontes, sin importar dónde o con quién estemos. Y adentrarnos
-                    en las páginas de un libro es el equivalente a subirnos en un avión del que solo nosotros elegimos
-                    el destino. Y porque los libros, a parte de diversión, nos aportan un montón de beneficios. ¿Cuáles?
-                    <a
-                        href="https://www.planetadelibros.com/blog/actualidad/15/nuestros-tops/11/articulo/beneficios-de-leer/237">
-                        Leer
-                        más</a>
+        <a class="terrarium" href="https://codepen.io/oliviale/full/oNXJgEM" target="_blank">
+            <figure>
+                <img src="{{ asset('images/home/library.jpg') }}" />
+                <figcaption> </figcaption>
+            </figure>
+        </a>
+        <a class="item-with-image plan span--2 long--2" href="https://codepen.io/oliviale/full/moLrBq" target="_blank">
+            <img src="{{ asset('images/home/power-of-literature.jpg') }}" />
+            <h4>¡Desbloquea el Poder de la Versión Pro! </h4>
+            <div class="multi-column">
+                <p><span class="font-bold">LetterKafka</span> es gratis, pero quizás te interesa la versión <span
+                        class="font-bold">PRO</span>. Una versión que va más allá, donde no
+                    habrá anuncios, donde tendrás un <span class="font-bold">wrapped</span> anual y ofertas únicas en
+                    tús
+                    librerías de confianza.
+                    Aparte
+                    desbloquearás una personalización de perfil más avanzada.
                 </p>
             </div>
-        </div>
-    </div>
-    <div class="sidebar">
-        <h3 class="title--big">Popular este mes</h3><a class="codepen-item pie"
-            href="https://codepen.io/oliviale/full/BaovGmg" target="_blank"><img class="pie__image"
-                src="https://books.google.es/books/publisher/content?id=VbB7DwAAQBAJ&hl=es&pg=PP1&img=1&zoom=3&bul=1&sig=ACfU3U0BtxRb7V8u-tOsMeHtBk7Xij41Mw&w=1280" />
-            <div class="pie__subtitle">Franz Kafka</div>
-            <div class="pie__content">
-                <p>En la quietud de la noche... se escuchaban solo susurros de misterio y promesa.</p>
-            </div>
-        </a><a class="sidebar-item captcha" href="https://codepen.io/oliviale/full/RwWdNRd" target="_blank">
-            <h5>Frustrated designer runs amok with Captcha ideas</h5>
-            <p>From Tic Tac Toe to solving meme-based questions, this is a different take on how web captchas
-                should be. "I have not successfully picked out all photos of a truck on the first try.
-                Something's gotta change," says the designer, who has requested to remain anonymous.</p>
-        </a><a class="sidebar-item slack-ui with-border" href="https://codepen.io/oliviale/full/GRpvNBa"
-            target="_blank">
-            <h5>Slack UI gets reverse engineered</h5>
-            <p>Another valiant effort to reverse engineer a web app. However, the UI is repurposed to showcase
-                Codepens instead of mock conversations. This is a codepen showcase inception situation.</p>
-        </a><a class="workout" href="https://codepen.io/oliviale/full/RwWKybY" target="_blank">
-            <div class="workout__image"><img
-                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-workout.jpg" alt="Workout" />
-            </div>
-            <div class="workout__blurb">Always failing to keep track of your workouts?</div>
-            <div class="workout__title">Use this tool!</div>
         </a>
-    </div>
+        <a class="hogwarts" href="https://codepen.io/oliviale/full/MZZYyO" target="_blank">
+            <div class="hogwarts__title">25% Menos en la versión PRO</div>
+            <div class="hogwarts__image">
+                <span>Oferta limitada</span>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/showcase-hogwarts.svg" />
+            </div>
+        </a>
+        <a class="item-with-image pasta with-border" href="https://codepen.io/oliviale/full/zYvxXWw" target="_blank">
+            <h4>GRATIS</h4>
+            <ul>
+                <li>Acceso <span class="font-bold">Básico</span> a la Biblioteca</li>
+                <li>Publicidad</li>
+                <li><span class="font-bold">Actualizaciones</span> Limitadas</li>
+                <li><span class="font-bold">Listas</span> limitadas</li>
+                <li>Recomendaciones de libros segun tus gustos</li>
+            </ul>
+        </a>
+        <a class="item-with-image magazine with-border" href="#" target="_blank">
+            <h4>PRO</h4>
+            <ul>
+                <li>No <span class="font-bold">ADS</span></li>
+                <li><span class="font-bold">Wrapped</span> Anual</li>
+                <li>Ofertar <span class="font-bold">Únicas</span> en Libros</li>
+                <li>Listas <span class="font-bold">Ilimitadas</span></li>
+                <li><span class="font-bold">Grupos </span>de lectura</li>
+            </ul>
+        </a>
+        <a class="item-with-image style" href="#" target="_blank">
+            <h4>¿Qué autor eres según tu personalidad? </h4>
+            <p>Nuestro test está diseñado para ayudarte a <span class="font-bold">descubrirlo</span>. Responde a unas
+                cuantas preguntas simples, sobre tus preferencias y rasgos de personalidad, y obtén resultados
+                personalizados que revelarán qué <span class="font-bold">autor</span> y estilo de escritura se alinean
+                mejor con tu perfil único. ¡Sumérgete en el mundo de la literatura y descubre nuevas afinidades
+                literarias con nuestro emocionante test!</p>
+        </a>
+        <a class="item-with-image toggles" href="#" target="_blank">
+            <img src="https://i.gifer.com/UK0h.gif" />
+            <h4>Grupos de lectura</h4>
+            <p>Los grupos de lectura son una característica destacada de nuestra aplicación, que ofrece a los usuarios
+                la oportunidad de conectarse con otros amantes de la lectura para <span
+                    class="font-bold">discutir</span>, <span class="font-bold">compartir</span> y <span
+                    class="font-bold">descubrir</span> nuevas perspectivas sobre los libros.</p>
+        </a>
+        <a class="menu" href="#" target="_blank">
+            <figure>
+                <img src="{{ asset('images/home/test-author.webp') }}" />
+                <figcaption>Descubre qué autor se adapta mejor a tu personalidad realizando nuestro test</figcaption>
+            </figure>
+        </a>
+        <a class="social" href="#" tagrget="_blank"><img class="social__image"
+                src="https://i.gifer.com/CWiP.gif" />
+            <div class="social__subtitle">curiosidad de hoy </div>
+            <div class="social__content">El libro impreso más antiguo que se conoce es el "Sutra del Diamante", un texto
+                budista que fue impreso en China en el año 868 d.C.</div>
+        </a>
 
+        <x-blogs-aleatorio />
+
+        <div class="sidebar">
+            <x-hot-book />
+            <div class="sidebar-item slack-ui with-border" href="#" target="_blank">
+                <h5>¿Quién es Franz Kafka?</h5>
+                <p>Fue un escritor de origen checo nacido en Praga en 1883 y fallecido en 1924. Es conocido por ser uno
+                    de los autores más influyentes del siglo XX y por su estilo literario único y surrealista. Kafka
+                    exploró temas como el <span class="font-bold">absurdo</span>, la <span
+                        class="font-bold">alienación</span> y la <span class="font-bold">burocracia</span> en sus obras,
+                    que a menudo presentaban personajes atrapados en situaciones surrealistas y opresivas.
+                    <a href="#" target="_blank">
+                        Leer más</a>
+                </p>
+            </div>
+            <a class="workout" href="https://es.wix.com/blog/2019/04/guia-para-crear-paginas-web/" target="_blank">
+                <div class="workout__image"><img
+                        src="https://static.wixstatic.com/media/72c0b2_087c223b84254953be07e3b5a9f71f5b~mv2.png/v1/fill/w_250,h_450,al_c,q_85,enc_auto/es03.png"
+                        alt="Ad wix" />
+                </div>
+                <div class="workout__blurb">¿Quieres crear un sitio web?</div>
+                <div class="workout__title">¡Usa esto!</div>
+            </a>
+        </div>
+    </main>
 </x-kafka-layout>
