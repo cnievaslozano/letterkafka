fa183ffff75ca8910eca34258642da669a1a9e3b
cnievaslozano <cnievaslozano@gmail.com>
2024-05-11 00:36:46 +0200

    routes libro show & mi perfil

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibrosController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\EstanteriasController;
+use App\Http\Controllers\UserController;
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +25,10 @@ Route::get('/', function () {
     return view('home');
 });
 Route::get('libros', [LibrosController::class, 'index'])->name('libros.index');
+Route::get('libros/{titulo}/{id}', [LibrosController::class, 'show'])->name('libros.show');
 Route::get('feed', [FeedController::class, 'index'])->name('feed.index');
 Route::get('mis-estanterias', [EstanteriasController::class, 'index'])->name('estanterias.index');
+Route::get('mi-perfil', [UserController::class, 'perfil'])->name('user.perfil');
 
 // FOOTER ROUTES
 Route::get('sobre-nosotros', function () {
@@ -34,7 +40,6 @@ Route::get('politica-privacidad', function () {
 Route::get('contacto', function () {
     return view('contacto');
 });
-
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
