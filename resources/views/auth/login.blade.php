44cdaa1a7dc32b90d153502b23de7037b093bad2
RumanoLoco <andreigeorgemira@gmail.com>
2024-05-14 13:15:49 +0200

    MIDDLEWARE OUT

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,15 +33,18 @@
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+{{--                @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                @endif--}}
 
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('register') }}">
+                    <span>¿No tienes cuenta?</span>
+                </a>
                 <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
--- routes/web.php
+++ routes/web.php
@@ -68,8 +68,3 @@ Route::get('contacto', function () {
     return view('contacto');
 })->name('contacto');
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
