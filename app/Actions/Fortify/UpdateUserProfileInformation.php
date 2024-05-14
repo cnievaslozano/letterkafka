ecbad63fc3b05724ad3611e52d7f6fad01771e80
cnievaslozano <cnievaslozano@gmail.com>
2024-05-14 02:26:10 +0200

    fix jetstream update photo

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
+use Laravel\Fortify\Features\UpdateProfilePhotos;
 
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
+    use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
@@ -21,23 +23,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
-    ];
+    protected $hidden = ['password', 'remember_token', 'two_factor_recovery_codes', 'two_factor_secret'];
 
     /**
      * The attributes that should be cast.
@@ -53,9 +46,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $appends = [
-        'profile_photo_url',
-    ];
+    protected $appends = ['profile_photo_url'];
 
     /**
      * Get the book lists owned by the user.
@@ -105,10 +96,6 @@ class User extends Authenticatable
         return $this->hasMany(Review::class);
     }
 
-    public function getProfilePhotoUrlAttribute(){
-        return "hello";
-    }
-
     public function contactMessages()
     {
         return $this->hasMany(ContactMessage::class);
@@ -119,5 +106,4 @@ class User extends Authenticatable
         $username = strtolower(str_replace(' ', '', $username)); // Convertir a minúsculas y eliminar espacios en blanco
         return $query->whereRaw('LOWER(REPLACE(name, " ", "")) LIKE ?', ['%' . $username . '%']);
     }
-
 }
